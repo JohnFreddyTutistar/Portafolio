135a1411522ef10b75d23fa10ca0277f374371b2
JohnFreddyTutistar <johnfre.157@gmail.com>
2020-12-26 20:59:00 -0500

    First commit

---
+++ js/dinam.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if(this,scrollY > 20){
+            $('.navcls_1').addClass("sticky");
+        }else{
+            $('.navcls_1').removeClass("sticky");
+        }
+    })
+
+    // alterar el menu
+    $('.menu-btn').click(function(){
+        $('.navcls_1 .menu').toogleClass("active");
+        $('.menu-btn i').toogleClass("active");
+    })
+})
